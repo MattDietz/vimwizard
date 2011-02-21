e1f0fa3727df36f3db1140344236a1a6f2133755
Matt Dietz <matt.dietz@rackspace.com>
2011-02-20 23:39:02 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vimwizard::Application.config.secret_token = 'cd2fa6cffc452dbabb9bfeb464efc5a5a28f910f54b56384cafee2652aa8f17534edbae686f32801f366e29f44aca8a718d85acd640b5c8224dd26187020fc6f'
